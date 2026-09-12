401cbffb659312f263dd0580d956d6ebef70c415
S.P. Vishwakarma <itspratap255@gmail.com>
2026-09-12 18:21:41 +0530

    add serializers

--- students/views.py
+++ students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def students(request):
+    students = [
+    {"name": "Pritam", "age": 22, "id": 1}
+    ]
+    return HttpResponse(students)
