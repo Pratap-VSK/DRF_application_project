b6f756cb894d6a56262595cb2076b6792e9c885a
S.P. Vishwakarma <itspratap255@gmail.com>
2026-09-14 22:53:00 +0530

    Class based serializers

--- api/views.py
+++ api/views.py
@@ -13,8 +13,36 @@ def studentsView(request):
         students = Student.objects.all()
         serializer = StudentSerializer(students, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    # elif request.method == 'POST':
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        print(serializer.errors)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def studentDetailView(request, pk):
+    try:
+        student = Student.objects.get(pk=pk)
+    except Student.DoesnotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = StudentSerializer(student)
+        return Response(status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    elif request.method == 'DELETE':
+        student.delete()
+        return Response(status=status.HTTP_301_MOVED_PERMANENTLY)
 
 
-
-   
+    
